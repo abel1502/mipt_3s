61ff08c90fb3926c3eec25e938209917c23ebf84
abel1502 <terragame.abel@gmail.com>
2021-10-13 19:57:22 +0300

    Physics implement molecule

--- Physics/molecule.cpp
+++ Physics/molecule.cpp
@@ -1,1 +1,236 @@
 #include "molecule.h"
+#include "manager.h"
+#include <typeinfo>
+
+
+Molecule Molecule::ManagerProxy::copy(const Molecule &self) {
+    return Molecule(self);
+}
+
+void Molecule::ManagerProxy::updateComponentBackrefs(Molecule &self) {
+    #define UPDATE_BACKREF_(COMP_TYPE) \
+        self.getComp<COMP_TYPE>().updateObj(&self);
+
+    UPDATE_BACKREF_(  PhysComp)
+    UPDATE_BACKREF_(RenderComp)
+    UPDATE_BACKREF_(  ChemComp)
+
+    #undef UPDATE_BACKREF_
+}
+
+Molecule::Molecule(MoleculeManager *manager_, const Vector2d &pos, double size, Preset preset) :
+    manager{manager_}, phys{nullptr}, rend{nullptr}, chem{nullptr}, flags{true, false} {
+
+    assert(abel::sgnDbl(size) > 0);
+
+    static constexpr double MASS_COEFF = 1.d;
+
+    switch (preset) {
+    case P_BALL: {
+        phys = new         PhysComp(this, pos, size * size * MASS_COEFF, size);
+        rend = new CircleRenderComp(this, size);
+        chem = new         ChemComp(this, 1);
+    } break;
+
+    case P_SQUARE: {
+        unsigned potency = 2;
+        if (size > 2.d) {
+            potency += (int)(size - 2.d);
+        }
+
+        phys = new         PhysComp(this, pos, size * size * MASS_COEFF, size);
+        rend = new SquareRenderComp(this, 2 * size);
+        chem = new         ChemComp(this, potency);
+    } break;
+
+    NODEFAULT
+    }
+}
+
+Molecule::Molecule(MoleculeManager *manager_, const PhysComp &phys_, const RenderComp &rend_, const ChemComp &chem_) :
+    Molecule(manager_, phys_.copy(), rend_.copy(), chem_.copy()) {
+
+    validate();
+}
+
+Molecule::Molecule(MoleculeManager *manager_, PhysComp *phys_, RenderComp *rend_, ChemComp *chem_) :
+    manager{manager_}, phys{phys_}, rend{rend_}, chem{chem_}, flags{true, false} {
+
+    validate();
+    phys->updateObj(this);
+    rend->updateObj(this);
+    chem->updateObj(this);
+}
+
+Molecule::Molecule(Molecule &&other) :
+    manager{other.manager}, phys{other.phys}, rend{other.rend}, chem{other.chem}, flags{other.flags} {
+
+    other.flags.inactive = false;
+    other.flags.dead     = false;
+
+    other.phys = nullptr;
+    other.rend = nullptr;
+    other.chem = nullptr;
+
+    phys->updateObj(this);
+    rend->updateObj(this);
+    chem->updateObj(this);
+}
+
+Molecule &Molecule::operator=(Molecule &&other) {
+    std::swap(manager, other.manager);
+    std::swap(phys,  other.phys);
+    std::swap(rend,  other.rend);
+    std::swap(chem,  other.chem);
+    std::swap(flags, other.flags);
+
+    if (phys)  phys->updateObj(this);
+    if (rend)  rend->updateObj(this);
+    if (chem)  chem->updateObj(this);
+
+    return *this;
+}
+
+Molecule::~Molecule() noexcept {
+    destroy();
+}
+
+void Molecule::update(double deltaT) {
+    validate();
+
+    phys->update(deltaT);
+
+    for (Border &border : getManager().getBorders()) {
+        if (!phys->testForBorderCollision(border))
+            continue;
+
+        phys->respondToBorderCollision(border);
+    }
+
+    // Other components don't currently have an update() method
+}
+
+void Molecule::updatePair(Molecule &other) {
+    bool colliding = getComp<PhysComp>().updatePair(other.getComp<PhysComp>());
+
+    if (isDead() || other.isDead())
+        return;
+
+    getComp<ChemComp>().updatePair(other.getComp<ChemComp>(), colliding);
+}
+
+void Molecule::render(Texture &target, const Coords &coords) {
+    getComp<RenderComp>().render(target, coords, getComp<PhysComp>().getTypeColor());
+}
+
+void Molecule::onBorderCollision(const Border &) {
+    // For now we do nothing here
+}
+
+Molecule &Molecule::onChemAbsorb(Molecule &other, unsigned potency) {
+    validate();
+    other.validate();
+
+    Molecule &survivor = phys->absorb(*other.phys);
+
+    if (&survivor == this) {
+        other.markDead();
+    } else if (&survivor == &other) {
+        markDead();
+    } else {
+        DBG("Shouldn't be possible");
+        REQUIRE(false);
+    }
+
+    survivor.getComp<RenderComp>().scaleTo(survivor.getComp<PhysComp>().getRadius() * 2);
+
+    if (potency > 1) {
+        survivor.rend = survivor.rend->becomeSquare();
+    } else {
+        survivor.rend = survivor.rend->becomeCircle();
+    }
+
+    std::swap(survivor.chem, chem);  // So that out chem holds the correct
+
+    survivor.phys->updateObj(&survivor);
+    survivor.rend->updateObj(&survivor);
+    survivor.chem->updateObj(&survivor);
+
+    return survivor;
+}
+
+void Molecule::renderBecomeCircle() {
+    rend = rend->becomeCircle();
+}
+
+void Molecule::renderBecomeSquare() {
+    rend = rend->becomeSquare();
+}
+
+void Molecule::scaleTrue(double coeff) {
+    validate();
+
+    //phys->scaleTrue(coeff);
+    //rend->scaleTrue(coeff);
+    //chem->scaleTrue(coeff);
+}
+
+void Molecule::dump() const noexcept {
+    DBG("Molecule (%p) {\n"
+        "  manager = %p\n"
+        "  phys    = %p (%s)\n"
+        "  rend    = %p (%s)\n"
+        "  chem    = %p (%s)\n"
+        "  flags   = dead:%d, inactive:%d\n"
+        "}",
+        this,
+        manager,
+        phys, typeid(phys).name(),
+        rend, typeid(rend).name(),
+        chem, typeid(chem).name(),
+        flags.dead, flags.inactive);
+}
+
+void Molecule::destroy() noexcept {
+    // The manager field remains
+
+    if (phys)
+        delete phys;
+    if (rend)
+        delete rend;
+    if (chem)
+        delete chem;
+
+    phys = nullptr;
+    rend = nullptr;
+    chem = nullptr;
+
+    flags.inactive = false;
+    flags.dead     = false;
+}
+
+Molecule::Molecule(const Molecule &other) :
+    manager{other.manager}, phys{nullptr}, rend{nullptr}, chem{nullptr}, flags{true, false} {
+
+    other.validate();
+
+    phys = other.phys->copy();
+    rend = other.rend->copy();
+    chem = other.chem->copy();
+}
+
+Molecule &Molecule::operator=(const Molecule &other) {
+    destroy();
+
+    other.validate();
+
+    flags.inactive = true;
+    flags.dead     = false;
+
+    manager = other.manager;
+    phys = other.phys->copy();
+    rend = other.rend->copy();
+    chem = other.chem->copy();
+
+    return *this;
+}
--- Physics/molecule.h
+++ Physics/molecule.h
@@ -3,45 +3,150 @@
 
 #include <ACL/general.h>
 #include <ACL/math/vector.h>
+#include <cassert>
+#include <type_traits>
 
 #include "components/physics.h"
 #include "components/render.h"
 #include "components/chemistry.h"
+#include "borders.h"
 
 
 using abel::math::Vector2d;
 
 
+class MoleculeManager;
+class Texture;
+class Coords;
+
 class Molecule {
 public:
+    // Sadly, a proxy like this is needed, since MoleculeManager's methods cannot be forward-declared here
+    class ManagerProxy {
+    protected:
+        friend class MoleculeManager;
+
+        static Molecule copy(const Molecule &self);
+
+        static void updateComponentBackrefs(Molecule &self);
+    };
+
+    static constexpr bool VALIDATE_PTRS = true;
+
     enum Preset {
         P_BALL,
         P_SQUARE,
     };
 
+    struct Flags {
+        bool inactive : 1;
+        bool dead     : 1;
+    };
 
-    Molecule();
 
-    Molecule(const Vector2d &pos, Preset preset);
+    Molecule(MoleculeManager *manager_, const Vector2d &pos = Vector2d{0, 0}, double size = 1.d, Preset preset = P_BALL);
+
+    /// This will copy the components
+    Molecule(MoleculeManager *manager_, const PhysComp &phys_, const RenderComp &rend_, const ChemComp &chem_);
+
+    /// This will take over the components.
+    /// They MUST be in dynamic memory, allocated with new
+    Molecule(MoleculeManager *manager_, PhysComp *phys_, RenderComp *rend_, ChemComp *chem_);
+
+    Molecule(Molecule &&other);
+    Molecule &operator=(Molecule &&other);
+
+    ~Molecule() noexcept;
 
     void update(double deltaT);
+
+    void updatePair(Molecule &other);
+
+    void render(Texture &target, const Coords &coords);
+
+    // The macro just acts a glorified comment
+    #define SOURCE_COMP_(NAME)
+
+    SOURCE_COMP_(PhysComp)
+    void onBorderCollision(const Border &border);
+
+    SOURCE_COMP_(ChemComp)
+    Molecule &onChemAbsorb(Molecule &other, unsigned potency);
+
+    #undef SOURCE_COMP_
+
+    void renderBecomeCircle();
+
+    void renderBecomeSquare();
+
+    /// Scales every aspect of the molecule as if it became `coeff` times larger in mass,
+    /// while retaining all other properties of the material, shape & etc
+    void scaleTrue(double coeff);
 
     /// Not defined for the generic case, since it only features present components
     template <typename T>
     T &getComp();
 
+    /// Same
+    template <typename T>
+    constexpr const T &getComp() const;
+
+    constexpr const Vector2d &getPos() const { validate(); return phys->getPos(); }
+    constexpr       Vector2d &getPos()       { validate(); return phys->getPos(); }
+
+    constexpr void markDead    (bool dead     = true) noexcept { flags.dead     = dead;     }
+    constexpr void markInactive(bool inactive = true) noexcept { flags.inactive = inactive; }
+
+    constexpr bool isDead()     const { return flags.dead;     }
+    constexpr bool isInactive() const { return flags.inactive; }
+
+    constexpr const MoleculeManager &getManager() const { validate(); return *manager; }
+    constexpr       MoleculeManager &getManager()       { validate(); return *manager; }
+
+    void dump() const noexcept;
+
 protected:
-    Vector2d pos;
-    PhysComp *phys;
-    RenderComp *render;
-    ChemComp *chem;
+    MoleculeManager *manager;
+
+    /// Cannot be nullptr unless the molecule is malformed
+    // (although the destructor should obviously be able to
+    //  handle even the malformed molecules)
+    PhysComp   *phys;
+    RenderComp *rend;
+    ChemComp   *chem;
+
+    Flags flags;
+
+    void destroy() noexcept;
+
+    inline constexpr void validate() const {
+        if constexpr (VALIDATE_PTRS) {
+            assert(manager);
+            assert(phys);
+            assert(rend);
+            assert(chem);
+        }
+    }
+
+    friend class ManagerProxy;
+
+    // Only to be called by the manager, since these methods don't add the molecule to the list
+    Molecule(const Molecule &other);
+    Molecule &operator=(const Molecule &other);
+
+    // Not to be used at all, hopefully, but needed for std::swap, because otherwise it invokes the move-ctor on garbage
+    Molecule() :
+        manager{nullptr}, phys{nullptr}, rend{nullptr}, chem{nullptr}, flags{} {}
 
 };
 
 
-template <> inline PhysComp   &Molecule::getComp<PhysComp>  () { return *phys;   }
-template <> inline RenderComp &Molecule::getComp<RenderComp>() { return *render; }
-template <> inline ChemComp   &Molecule::getComp<ChemComp>  () { return *chem;   }
+template <> constexpr       PhysComp   &Molecule::getComp<PhysComp>  ()       { validate(); return *phys; }
+template <> constexpr const PhysComp   &Molecule::getComp<PhysComp>  () const { validate(); return *phys; }
+template <> constexpr       RenderComp &Molecule::getComp<RenderComp>()       { validate(); return *rend; }
+template <> constexpr const RenderComp &Molecule::getComp<RenderComp>() const { validate(); return *rend; }
+template <> constexpr       ChemComp   &Molecule::getComp<ChemComp>  ()       { validate(); return *chem; }
+template <> constexpr const ChemComp   &Molecule::getComp<ChemComp>  () const { validate(); return *chem; }
 
 
 #endif // MOLECULE_H
