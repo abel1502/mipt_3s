36311ee28d8180bf601ceac973e9de2e118154d7
abel1502 <terragame.abel@gmail.com>
2021-10-13 19:52:59 +0300

    Physics expand comp framework

--- Physics/components/component.cpp
+++ Physics/components/component.cpp
@@ -3,5 +3,13 @@
 #include "../molecule.h"
 
 
-Component::Component(Molecule &object_) noexcept :
-    object{object_} {}
+Component::Component(Molecule *object_) noexcept :
+    object{object_} {
+
+    // Can't assert this anymore, since random molecule creation involves having this at nullptr and then updating it
+    // assert(object_);
+}
+
+void Component::updateObj(Molecule *new_object) noexcept {
+    object = new_object;
+}
--- Physics/components/component.h
+++ Physics/components/component.h
@@ -1,18 +1,28 @@
 #ifndef COMPONENTS_COMPONENT_H
 #define COMPONENTS_COMPONENT_H
 
+#include <cassert>
+
 
 class Molecule;
 
+/// Warning: not suitable for actual use, since this is NOT a polymorphic base class.
+/// Only provides some common methods.
+/// Preferable this way, since some components may not need to be polymorphic, so they could be sped up this way
 class Component {
 public:
-    Component(Molecule &object_) noexcept;
+    Component(Molecule *object_) noexcept;
+
+    constexpr const Molecule &getObj() const noexcept { assert(object); return *object; }
+    constexpr       Molecule &getObj()       noexcept { assert(object); return *object; }
+
+    void updateObj(Molecule *new_object) noexcept;
 
     /// Not virtual, no implementation - just an interface guideline
-    Component *copy();
+    Component *copy() const;
 
 protected:
-    Molecule &object;
+    Molecule *object;
 
 };
 
