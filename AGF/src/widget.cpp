ee631de91abf8ab465dd17d28d8fd5d490f9a626
abel1502 <terragame.abel@gmail.com>
2021-10-20 22:49:37 +0300

    AGF implement Widget...?

---
+++ AGF/src/widget.cpp
@@ -0,0 +1,62 @@
+#include <AGF/widget.h>
+
+
+namespace abel::gui {
+
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+EVENT_HANDLER_IMPL(Widget, Render) {
+    if (!visible)
+        return E_CONTINUE;
+
+    return E_SKIP;
+}
+
+EVENT_HANDLER_IMPL(Widget, Resize) {
+    region = event.newRegion;
+
+    return E_CONTINUE;
+}
+
+EVENT_HANDLER_IMPL(Widget, Move) {
+    return E_SKIP;
+}
+
+EVENT_HANDLER_IMPL(Widget, ActiveStatusUpdate) {
+    return E_SKIP;
+}
+
+EVENT_HANDLER_IMPL(Widget, Start) {
+    return E_SKIP;
+}
+
+EVENT_HANDLER_IMPL(Widget, Exit) {
+    return E_SKIP;
+}
+
+EVENT_HANDLER_IMPL(Widget, MouseClick) {
+    return E_SKIP;
+}
+
+EVENT_HANDLER_IMPL(Widget, MouseMove) {
+    return E_SKIP;
+}
+
+EVENT_HANDLER_IMPL(Widget, Keyboard) {
+    return E_SKIP;
+}
+
+EVENT_HANDLER_IMPL(Widget, User) {
+    return E_SKIP;
+}
+
+#pragma GCC diagnostic pop
+
+
+Widget::Widget(Widget *parent_, const Rect<double> &region_) :
+    parent{parent_}, region{region_} {}
+
+
+}
