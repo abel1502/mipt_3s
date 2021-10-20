4441420b6deda4a5ed61e3bd3cafc539e36b05bd
abel1502 <terragame.abel@gmail.com>
2021-10-20 22:44:45 +0300

    ACL modify tests

--- ACL_Tests/main.cpp
+++ ACL_Tests/main.cpp
@@ -2,22 +2,39 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cassert>
-#include <ACL/event.h>
+#include <ACL/signal.h>
+#include <ACL/list.h>
+
+//#define UNSUB_
 
 
+#ifdef UNSUB_
+bool callback() {
+    DBG("Called back!");
+
+    return true;
+}
+#else
 void callback() {
     DBG("Called back!");
 }
+#endif
 
 
 int main() {
     abel::verbosity = 2;
 
-    abel::Event<void()> event{};
+    #ifdef UNSUB_
+    abel::Signal<bool ()> event{};
+    #else
+    abel::Signal<void ()> event{};
+    #endif
 
-    event.subscribe(callback);
+    event += callback;
+    event += callback;
 
-    event.invoke();
+    event();
+    event();
 
     return 0;
 }
