8cda75f30229c76c2fdfed9ee6575b080b4caccf
abel1502 <terragame.abel@gmail.com>
2021-10-31 21:39:53 +0300

    Change photoshoop sample code

--- Photoshoop/app.cpp
+++ Photoshoop/app.cpp
@@ -9,23 +9,32 @@ abel::gui::Application::app_ptr_t abel::gui::Application::create() {
 
 
 MyApp::MyApp() :
-    Application() {
-
-    mainWidget = new abel::gui::widgets::SimpleButton(nullptr, abel::gui::Rect<double>::wh(20, 20, 100, 60), "Button");
-}
+    Application() {}
 
 
 MyApp::~MyApp() = default;
 
 
 void MyApp::init(int argc, const char **argv) {
+    if (initialized)
+        return;
+
     abel::verbosity = 2;
+
+    abel::unique_ptr<abel::gui::widgets::SimpleButton> btn{};
+    btn.emplace(nullptr, abel::gui::Rect<double>::wh(20, 20, 100, 60), "Button");
+    btn->sigClick += [](){ static unsigned cnt = 3; DBG("Knopochka"); return !(--cnt); };
+    mainWidget = std::move(btn);
+
     Application::init(argc, argv);
     DBG("");
 }
 
 
 void MyApp::deinit() {
+    if (!initialized)
+        return;
+
     Application::deinit();
     DBG("");
 }
