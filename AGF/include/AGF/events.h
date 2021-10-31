0a63d9741458b8cea7587daeddadfb1110f79b3d
abel1502 <terragame.abel@gmail.com>
2021-10-31 21:29:04 +0300

    AGF implement some events

--- AGF/include/AGF/events.h
+++ AGF/include/AGF/events.h
@@ -20,18 +20,38 @@ namespace abel::gui {
 
 
 EVENT_CLS_DECL_(Render) {
-    Rect<double> region;
+    Rect<double> region;  // Each widget, when forwarding this event to its children,
+                          // should reset the region to be its own, so that they get to have relative addresses
     Texture &target;
 
     constexpr EVENT_CLS_NAME(Render)(const Rect<double> &region_, Texture &target_) :
         region{region_}, target{target_} {}
+
+    constexpr EVENT_CLS_NAME(Render) createSubEvent(const Rect<double> &subRegion_) {
+        EVENT_CLS_NAME(Render) subEvent{*this};
+
+        subEvent.region = subRegion_;
+
+        return subEvent;
+    }
 };
 
 EVENT_CLS_DECL_(Move) {};
 
 /// May involve movement as well
 EVENT_CLS_DECL_(Resize) {
-    Rect<double> newRegion;
+    Rect<double> newRegion;  // This refers to the new size of the widget that gets this event
+
+    constexpr EVENT_CLS_NAME(Resize)(const Rect<double> &newRegion_) :
+        newRegion{newRegion_} {}
+
+    constexpr EVENT_CLS_NAME(Resize) createSubEvent(const Rect<double> &subNewRegion_) {
+        EVENT_CLS_NAME(Resize) subEvent{*this};
+
+        subEvent.newRegion = subNewRegion_;
+
+        return subEvent;
+    }
 };
 
 EVENT_CLS_DECL_(ActiveStatusUpdate) {};
@@ -45,6 +65,17 @@ EVENT_CLS_DECL_(MouseClick) {
     MouseAttrs attrs;
     MouseBtn button;
     MouseClickType type;
+
+    constexpr EVENT_CLS_NAME(MouseClick)(const Vector2d &pos_, const MouseAttrs &attrs_, MouseBtn button_, MouseClickType type_) :
+        pos{pos_}, attrs{attrs_}, button{button_}, type{type_} {}
+
+    constexpr EVENT_CLS_NAME(MouseClick) createSubEvent(const Vector2d &pos_) {
+        EVENT_CLS_NAME(MouseClick) subEvent{*this};
+
+        subEvent.pos = pos_;
+
+        return subEvent;
+    }
 };
 
 EVENT_CLS_DECL_(MouseMove) {
