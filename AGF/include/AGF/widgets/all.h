4668ea642a037afe5abce691907a88901928abe3
abel1502 <terragame.abel@gmail.com>
2021-11-07 00:35:01 +0300

    AGF add all.h

---
+++ AGF/include/AGF/widgets/all.h
@@ -0,0 +1,10 @@
+#ifndef AGF_WIDGETS_ALL_H
+#define AGF_WIDGETS_ALL_H
+
+#include <AGF/widgets/group.h>
+#include <AGF/widgets/static_group.h>
+#include <AGF/widgets/button.h>
+#include <AGF/widgets/label.h>
+#include <AGF/widgets/rectangle.h>
+
+#endif // AGF_WIDGETS_ALL_H
