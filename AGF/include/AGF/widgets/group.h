0cc0f150dc513840a52c8dbe35bc6f3b1d555249
abel1502 <terragame.abel@gmail.com>
2021-11-08 00:01:22 +0300

    AGF templategize Group

--- AGF/include/AGF/widgets/group.h
+++ AGF/include/AGF/widgets/group.h
@@ -3,25 +3,34 @@
 
 #include <AGF/widget.h>
 #include <ACL/list.h>
-// #include <ACL/debug.h>
 
 
 namespace abel::gui::widgets {
 
 
+template <typename T>
+static constexpr bool eventCapturesFocus = false;
+
+// TODO: Maybe some other specific events as well?
+template <>
+constexpr bool eventCapturesFocus<MouseClickEvent> = true;
+
+
 // TODO: Maybe inherit virtually
-class Group : public /*virtual*/ Widget {
+template <typename ITEM>
+class GroupOf : public /*virtual*/ Widget {
 public:
     EVENT_HANDLER_USING(Widget)
 
-    Group(Widget *parent_, const Rect<double> &region_);
+    GroupOf(Widget *parent_, const Rect<double> &region_) :
+        Widget(parent_, region_) {}
 
     template <typename T, typename ... As>
     T &createChild(const Rect<double> &relRegion, As &&... args) {
-        static_assert(std::is_base_of_v<Widget, T>);
+        static_assert(std::is_base_of_v<ITEM, T>);
 
         // TODO: Maybe disallow overflow?
-        Widget &child = *children.insertBack(new T(this, region.relRect(relRegion, true), std::forward<As>(args)...));
+        ITEM &child = *children.insertBack(new T(this, region.relRect(relRegion, true), std::forward<As>(args)...));
 
         return dynamic_cast<T &>(child);
     }
@@ -31,19 +40,69 @@ public:
     #include <AGF/events.dsl.h>
 
 protected:
-    list<unique_ptr<Widget>> children{};
+    list<unique_ptr<ITEM>> children{};
 
-    template <typename T>
-    static constexpr bool focusOnEvent = false;
 
     // TODO: A public alternative
-    void addChild(Widget *child);
+    void addChild(ITEM *child) {
+        assert(child);
+        children.insertBack(child);
+        child->updateParent(this);
+    }
 
     // TODO: Same
-    void clearChildren();
+    void clearChildren() {
+        children.clear();
+    }
+
+    // Render event has to be handled separately, because it works in reverse
+    inline EventStatus _processEvent(const RenderEvent &event) {
+        EventStatus status = Widget::dispatchEvent(event);
+
+        if (!status.shouldHandle(status.NODE))
+            return status.update();
+
+        static_assert(RenderEvent::demands_modification);
+        RenderEvent subEvent = event.createSubEvent(region);
+
+        auto childrenEnd = children.end();  // We rely on our loop being cyclic
+        for (auto iter = --children.end(); iter != childrenEnd; --iter) {
+            auto &child = *iter;
+
+            status = child->dispatchEvent(subEvent);
+
+            if (!status.shouldHandle(status.SIBL))
+                break;
+        }
+
+        static_assert(!eventCapturesFocus<RenderEvent>);
+
+        return status.update();
+    }
+
+    // FocusUpdates should only be passed to the active child
+    inline EventStatus _processEvent(const FocusUpdateEvent &event) {
+        EventStatus status = Widget::dispatchEvent(event);
+
+        if (!status.shouldHandle(status.NODE))
+            return status.update();
+
+        if (children.isEmpty())
+            return status.update();
+
+        status = dispatchToChild(*children.front(), event);
+
+        return status.update();
+    }
 
     template <typename T>
     inline EventStatus _processEvent(const T &event) {
+        // Sadly, full specializations aren't possible here...
+        // I guess I'll at least provide
+        if constexpr (std::is_same_v<T, RenderEvent> || std::is_same_v<T, FocusUpdateEvent>) {
+            return _processEvent(event);  // This is NOT recursion, but delegation to specific overloads!
+        }
+
         EventStatus status = Widget::dispatchEvent(event);
 
         if (!status.shouldHandle(status.NODE))
@@ -59,7 +118,7 @@ protected:
             ++targetChild;
         }
 
-        if constexpr (focusOnEvent<T>) {
+        if constexpr (eventCapturesFocus<T>) {
             if (targetChild != children.end()) {
                 focusChild(targetChild);
             }
@@ -68,57 +127,31 @@ protected:
         return status.update();
     }
 
-    void focusChild(const decltype(children)::iterator &child);
+    void focusChild(const typename decltype(children)::iterator &child)  {
+        children.front()->dispatchEvent(FocusUpdateEvent{false});
+
+        children.swapFront(child);
+
+        children.front()->dispatchEvent(FocusUpdateEvent{true});
+    }
 
 };
 
-// TODO: Maybe some other specific events as well?
-template <>
-constexpr bool Group::focusOnEvent<MouseClickEvent> = true;
 
-
-// Render event has to be handled separately, because it works in reverse
-template <>
-inline Widget::EventStatus Group::_processEvent(const RenderEvent &event) {
-    EventStatus status = Widget::dispatchEvent(event);
-
-    if (!status.shouldHandle(status.NODE))
-        return status.update();
-
-    static_assert(RenderEvent::demands_modification);
-    RenderEvent subEvent = event.createSubEvent(region);
-
-    auto childrenEnd = children.end();  // We rely on our loop being cyclic
-    for (auto iter = --children.end(); iter != childrenEnd; --iter) {
-        auto &child = *iter;
-
-        status = child->dispatchEvent(subEvent);
-
-        if (!status.shouldHandle(status.SIBL))
-            break;
+#define EVENTS_DSL_ITEM_(NAME)                  \
+    template <typename ITEM>                    \
+    EVENT_HANDLER_IMPL(GroupOf<ITEM>, NAME) {   \
+        return _processEvent(event);            \
     }
 
-    static_assert(!focusOnEvent<RenderEvent>);
-
-    return status.update();
-}
+#include <AGF/events.dsl.h>
 
 
-// FocusUpdates should only be passed to the active child
-template <>
-inline Widget::EventStatus Group::_processEvent(const FocusUpdateEvent &event) {
-    EventStatus status = Widget::dispatchEvent(event);
+extern template
+class GroupOf<Widget>;
 
-    if (!status.shouldHandle(status.NODE))
-        return status.update();
 
-    if (children.isEmpty())
-        return status.update();
-
-    status = dispatchToChild(*children.front(), event);
-
-    return status.update();
-}
+using Group = GroupOf<Widget>;
 
 
 }
--- AGF/src/widgets/group.cpp
+++ AGF/src/widgets/group.cpp
@@ -1,39 +1,11 @@
-#include <AGF/llgui.h>
-#include <ACL/general.h>
 #include <AGF/widgets/group.h>
 
 
 namespace abel::gui::widgets {
 
 
-Group::Group(Widget *parent_, const Rect<double> &region_) :
-    Widget(parent_, region_) {}
-
-void Group::addChild(Widget *child) {
-    assert(child);
-    children.insertBack(child);
-    child->updateParent(this);
-}
-
-void Group::clearChildren() {
-    children.clear();
-}
-
-void Group::focusChild(const decltype(children)::iterator &child) {
-    children.front()->dispatchEvent(FocusUpdateEvent{false});
-
-    children.swapFront(child);
-
-    children.front()->dispatchEvent(FocusUpdateEvent{true});
-}
-
-
-#define EVENTS_DSL_ITEM_(NAME)          \
-    EVENT_HANDLER_IMPL(Group, NAME) {   \
-        return _processEvent(event);    \
-    }
-
-#include <AGF/events.dsl.h>
+template
+class GroupOf<Widget>;
 
 
 }
