328ce4c1e3587f15d7b60a401f5c99685964c28d
abel1502 <terragame.abel@gmail.com>
2021-11-13 23:55:04 +0300

    ACL refactor Rect significantly

--- ACL/include/ACL/gui/rect.h
+++ ACL/include/ACL/gui/rect.h
@@ -12,33 +12,40 @@ namespace abel::gui {
 template <typename T>
 class Rect {
     //static_assert(std::is_arithmetic_v<T>);
-
 public:
+    using Vector2T = typename abel::math::Vector2<typename abel::make_signed_t<T>>;
+
+
     constexpr Rect() noexcept :
         x_{0}, y_{0}, w_{0}, h_{0} {}
 
     /// Width-height
-    constexpr Rect(T new_x, T new_y, T new_w, T new_h) noexcept {
-        x(new_x);
-        y(new_y);
-        w(new_w);
-        h(new_h);
-    }
-
     static constexpr Rect wh(T x, T y, T w, T h) noexcept {
         return Rect(x, y, w, h);
     }
 
     /// Start-end
-    constexpr Rect<T>(T new_x0, T new_y0, T new_x1, T new_y1, int) noexcept {
-        x0(new_x0);
-        y0(new_y0);
-        x1(new_x1);
-        y1(new_y1);
+    template <bool SORT = true>
+    static constexpr Rect se(T x0, T y0, T x1, T y1) noexcept {
+        if constexpr (SORT) {
+            if (x0 > x1)
+                std::swap(x0, x1);
+            if (y0 > y1)
+                std::swap(y0, y1);
+        }
+
+        return Rect(x0, y0, x1, y1, 0);
     }
 
-    static constexpr Rect se(T x0, T y0, T x1, T y1) noexcept {
-        return Rect(x0, y0, x1, y1, 0);
+    /// Width-height, vector
+    static constexpr Rect wh(const Vector2T &from, const Vector2T &size) noexcept {
+        return Rect::wh(from.x(), from.y(), size.x(), size.y());
+    }
+
+    /// Start-end, vector
+    template <bool SORT = true>
+    static constexpr Rect se(const Vector2T &start, const Vector2T &end) noexcept {
+        return Rect::se<SORT>(start.x(), start.y(), end.x(), end.y());
     }
 
     constexpr T  x() const noexcept { return x_; }
@@ -59,6 +66,25 @@ public:
     constexpr void x1(T val) noexcept { w_ = val - x_; }
     constexpr void y1(T val) noexcept { h_ = val - y_; }
 
+    // Pos and Start differ!
+    // Pos refers to x and y of wh, whereas Start refers to x0 and y0 of se
+    // Thus, setting them maintains either Diag or End, respectively
+
+    constexpr Vector2T getStart() const { return Vector2T(x0(), y0()); }
+    constexpr Vector2T getEnd  () const { return Vector2T(x1(), y1()); }
+    constexpr Vector2T getPos  () const { return Vector2T(x (), y ()); }
+    constexpr Vector2T getDiag () const { return Vector2T(w (), h ()); }
+
+    constexpr void setStart(const Vector2T &value) { x0(value.x()); y0(value.y()); }
+    constexpr void setEnd  (const Vector2T &value) { x1(value.x()); y1(value.y()); }
+    constexpr void setPos  (const Vector2T &value) { x (value.x()); y (value.y()); }
+    constexpr void setDiag (const Vector2T &value) { w (value.x()); h (value.y()); }
+
+    constexpr Vector2T getVertex(bool xCoord, bool yCoord) const {
+        return Vector2T(x() + (!!xCoord) * w(),
+                        y() + (!!yCoord) * h());
+    }
+
     template <typename T2>
     constexpr operator Rect<T2>() const {
         return Rect<T2>((T2)x(), (T2)y(), (T2)w(), (T2)h());
@@ -77,52 +103,6 @@ public:
         Rect result{*this};
 
         return result.intersect(bounds);
-    }
-
-
-    using Vector2T = typename abel::math::Vector2<typename abel::make_signed_t<T>>;
-
-    // Pos and Start differ!
-    // Pos refers to x and y of wh, whereas Start refers to x0 and y0 of se
-    // Thus, setting them maintains either Diag or End, respectively
-
-    constexpr Vector2T getStart() const { return Vector2T(x0(), y0()); }
-    constexpr Vector2T getEnd  () const { return Vector2T(x1(), y1()); }
-    constexpr Vector2T getPos  () const { return Vector2T(x (), y ()); }
-    constexpr Vector2T getDiag () const { return Vector2T(w (), h ()); }
-
-    constexpr void setStart(const Vector2T &value) { x0(value.x()); y0(value.y()); }
-    constexpr void setEnd  (const Vector2T &value) { x1(value.x()); y1(value.y()); }
-    constexpr void setPos  (const Vector2T &value) { x (value.x()); y (value.y()); }
-    constexpr void setDiag (const Vector2T &value) { w (value.x()); h (value.y()); }
-
-    constexpr Vector2T getVertex(bool xCoord, bool yCoord) const {
-        return Vector2T(x() + xCoord * w(),
-                        y() + yCoord * h());
-    }
-
-    /// Width-height, vector
-    constexpr Rect(const Vector2T &from, const Vector2T &size) noexcept {
-        x(from.x());
-        y(from.y());
-        w(size.x());
-        h(size.y());
-    }
-
-    static constexpr Rect wh(const Vector2T &from, const Vector2T &size) noexcept {
-        return Rect(from, size);
-    }
-
-    /// Start-end, vector
-    constexpr Rect(const Vector2T &from, const Vector2T &to, int) noexcept {
-        x0(from.x());
-        y0(from.y());
-        x1(to  .x());
-        y1(to  .y());
-    }
-
-    static constexpr Rect se(const Vector2T &start, const Vector2T &end) noexcept {
-        return Rect(start, end, 0);
     }
 
     constexpr bool contains(const Vector2T &point) const noexcept {
@@ -231,6 +211,23 @@ public:
 
 protected:
     T x_, y_, w_, h_;
+
+
+    /// Width-height
+    constexpr Rect(T new_x, T new_y, T new_w, T new_h) noexcept {
+        x(new_x);
+        y(new_y);
+        w(new_w);
+        h(new_h);
+    }
+
+    /// Start-end
+    constexpr Rect<T>(T new_x0, T new_y0, T new_x1, T new_y1, int) noexcept {
+        x0(new_x0);
+        y0(new_y0);
+        x1(new_x1);
+        y1(new_y1);
+    }
 
 };
 
