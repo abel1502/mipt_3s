0d361c5c759e306389a552f2b4ff1d72b04f012d
abel1502 <terragame.abel@gmail.com>
2021-11-30 12:14:30 +0300

    VS Photoshoop implement color_picker
    A big commit as well

---
+++ VS/Photoshoop/color_picker.cpp
@@ -0,0 +1,191 @@
+#include <AGF/llgui.h>
+#include <ACL/unique_ptr.h>
+#include "color_picker.h"
+
+
+using namespace abel;
+using namespace abel::gui;
+
+
+namespace _myimpl {
+
+void ColorSliderThumb::renderThumb(abel::gui::Texture &target, const Rect<double> &at) const {
+    target.setLineColor(Color::WHITE * 0.6f);
+    target.setLineWidth(1.5f);
+    target.drawEllipse(pos, at.getDiag() / 2, false);
+}
+
+void ColorSliderSV::renderBackground(Texture &target, const Rect<double> &at) const {
+    assert(parent);
+    double hue = getParent()->getColorH();
+
+    if (!cachedTexture || hue != cachedTextureHue) {
+        cachedTextureHue = hue;
+
+        unsigned w = (unsigned)at.w();
+        unsigned h = (unsigned)at.h();
+
+        w = std::min(w, 64u);
+        h = std::min(h, 64u);
+        assert(w && h);
+
+        cachedTexture.emplace(w, h);
+
+        PackedColor *tmpBuf = cachedTexture->getBuf(false, true);
+        assert(tmpBuf);
+
+        unsigned numPixels = w * h;
+
+        unsigned x = 0;
+        unsigned y = 0;
+        for (unsigned i = 0; i < numPixels; ++i) {
+            x++;
+            if (x >= w) {
+                x = 0;
+                y++;
+            }
+
+            tmpBuf[y * w + x] = Window::HSV2RGB(
+                abel::math::Vector3d{hue, (double)x / w, 1 - (double)y / h}
+            ).pack();
+        }
+
+        cachedTexture->flushBuf();
+    }
+
+    target.embed(at, *cachedTexture);
+}
+
+void ColorSliderH::renderBackground(Texture &target, const Rect<double> &at) const {
+    if (!cachedTexture) {
+        unsigned w = (unsigned)at.w();
+        unsigned h = (unsigned)at.h();
+
+        w = std::min(w, 64u);
+        h = std::min(h, 16u);
+        assert(w && h);
+
+        cachedTexture.emplace(w, h);
+
+        PackedColor *tmpBuf = cachedTexture->getBuf(false, true);
+        assert(tmpBuf);
+
+        unsigned numPixels = w * h;
+
+        unsigned x = 0;
+        unsigned y = 0;
+        for (unsigned i = 0; i < numPixels; ++i) {
+            x++;
+            if (x >= w) {
+                x = 0;
+                y++;
+            }
+
+            tmpBuf[y * w + x] = Window::HSV2RGB(
+                abel::math::Vector3d{(double)x / w, 1., 1.}
+            ).pack();
+        }
+
+        cachedTexture->flushBuf();
+    }
+
+    target.embed(at, *cachedTexture);
+}
+
+void ColorSliderA::renderBackground(Texture &target, const Rect<double> &at) const {
+    if (!cachedTexture) {
+        unsigned w = (unsigned)at.w();
+        unsigned h = (unsigned)at.h();
+
+        w = std::min(w, 64u);
+        h = std::min(h, 16u);
+        assert(w && h);
+
+        cachedTexture.emplace(w, h);
+
+        PackedColor *tmpBuf = cachedTexture->getBuf(false, true);
+        assert(tmpBuf);
+
+        unsigned numPixels = w * h;
+
+        unsigned x = 0;
+        unsigned y = 0;
+        for (unsigned i = 0; i < numPixels; ++i) {
+            x++;
+            if (x >= w) {
+                x = 0;
+                y++;
+            }
+
+            tmpBuf[y * w + x] = Color(Color::WHITE * (1.f - (float)x / w)).pack();
+        }
+
+        cachedTexture->flushBuf();
+    }
+
+    target.embed(at, *cachedTexture);
+}
+
+}
+
+
+ColorPicker::ColorPicker(Widget *parent_, const Rect<double> &region_) :
+    Base(parent_, region_,
+         nullptr,
+         nullptr,
+         nullptr) {
+
+    double height = region.h();
+
+    Rect<double> regionSV = region;
+    regionSV.h(height * 0.7);
+
+    Rect<double> regionH = region;
+    regionH.y(height * 0.75);
+    regionH.h(height * 0.1);
+
+    Rect<double> regionA = region;
+    regionA.y(height * 0.9);
+    regionA.h(height * 0.1);
+
+    sliderSV(new _myimpl::ColorSliderSV(this, regionSV, Rect<double>::wh(0, 1, 1, -1)));
+    sliderH (new _myimpl::ColorSliderH (this, regionH , 0, 1));
+    sliderA (new _myimpl::ColorSliderA (this, regionA , 0, 1));
+}
+
+
+Color ColorPicker::getColor() const {
+    return Window::HSV2RGB(abel::math::Vector3d(getColorH(), getColorS(), getColorV()));
+}
+
+void ColorPicker::setColor(const Color &value) {
+    abel::math::Vector3d hsl = Window::RGB2HSV(value);
+
+    setColorH(hsl.x());
+    setColorS(hsl.y());
+    setColorV(hsl.z());
+}
+
+double ColorPicker::getColorH() const {
+    return sliderH().getValue();
+}
+
+double ColorPicker::getColorS() const {
+    return sliderSV().getValue().x();
+}
+
+double ColorPicker::getColorV() const {
+    return sliderSV().getValue().y();
+}
+
+void ColorPicker::setColorH(double value) {
+    sliderH().setValue(value);
+}
+
+void ColorPicker::setColorS(double value) {
+    sliderSV().setValue({value, sliderSV().getValue().y()});
+}
+
+void ColorPicker::setColorV(double value) {
+    sliderSV().setValue({sliderSV().getValue().x(), value});
+}
---
+++ VS/Photoshoop/color_picker.h
@@ -0,0 +1,167 @@
+#pragma once
+
+#include <ACL/general.h>
+#include <AGF/widget.h>
+#include <AGF/widgets/all.h>
+
+
+using abel::gui::Rect;
+using abel::gui::Color;
+using abel::math::Vector2d;
+
+
+class ColorPicker;
+
+
+// Not simply _impl to be more friendly towards VS's intellisense
+namespace _myimpl {
+
+class ColorSliderThumb : public abel::gui::widgets::Thumb {
+public:
+    using Base = abel::gui::widgets::Thumb;
+    EVENT_HANDLER_USING(Base);
+
+
+    using Base::Thumb;
+
+protected:
+    virtual void renderThumb(abel::gui::Texture &target, const Rect<double> &at) const override;
+};
+
+class ColorSliderSV : public abel::gui::widgets::Slider2D {
+public:
+    using Base = abel::gui::widgets::Slider2D;
+    EVENT_HANDLER_USING(Base);
+
+
+    inline ColorSliderSV(ColorPicker *parent_, const Rect<double> &region_,
+                         const Rect<double> &limits_);
+
+    inline const ColorPicker *getParent() const;
+    inline       ColorPicker *getParent()      ;
+
+protected:
+    mutable double cachedTextureHue = NAN;
+    mutable abel::unique_ptr<abel::gui::Texture> cachedTexture = nullptr;
+
+
+    virtual void renderBackground(abel::gui::Texture &target, const Rect<double> &at) const override;
+
+};
+
+
+class ColorSliderH : public abel::gui::widgets::SliderH {
+public:
+    using Base = abel::gui::widgets::SliderH;
+    EVENT_HANDLER_USING(Base);
+
+
+    inline ColorSliderH(ColorPicker *parent_, const Rect<double> &region_,
+                        double min, double max);
+
+    inline const ColorPicker *getParent() const;
+    inline       ColorPicker *getParent()      ;
+
+protected:
+    mutable abel::unique_ptr<abel::gui::Texture> cachedTexture = nullptr;
+
+
+    virtual void renderBackground(abel::gui::Texture &target, const Rect<double> &at) const override;
+
+};
+
+
+class ColorSliderA : public abel::gui::widgets::SliderH {
+public:
+    using Base = abel::gui::widgets::SliderH;
+    EVENT_HANDLER_USING(Base);
+
+
+    inline ColorSliderA(ColorPicker *parent_, const Rect<double> &region_,
+                        double min, double max);
+
+    inline const ColorPicker *getParent() const;
+    inline       ColorPicker *getParent()      ;
+
+protected:
+    mutable abel::unique_ptr<abel::gui::Texture> cachedTexture = nullptr;
+
+
+    virtual void renderBackground(abel::gui::Texture &target, const Rect<double> &at) const override;
+
+};
+
+}  // namespace _myimpl
+
+
+class ColorPicker : public abel::gui::widgets::StaticGroup<
+                               _myimpl::ColorSliderSV,
+                               _myimpl::ColorSliderH,
+                               _myimpl::ColorSliderA
+                           > {
+public:
+    using Base = abel::gui::widgets::StaticGroup<
+                     _myimpl::ColorSliderSV,
+                     _myimpl::ColorSliderH,
+                     _myimpl::ColorSliderA
+                 >;
+    EVENT_HANDLER_USING(Base);
+
+    ColorPicker(Widget *parent_, const Rect<double> &region_);
+
+    Color getColor() const;
+    void setColor(const Color &value);
+
+    inline double getAlpha() const;
+    inline void setAlpha(double value);
+
+protected:
+    friend class _myimpl::ColorSliderSV;
+    friend class _myimpl::ColorSliderH;
+    friend class _myimpl::ColorSliderA;
+
+    SGRP_DECLARE_BINDING_T(sliderSV, _myimpl::ColorSliderSV);
+    SGRP_DECLARE_BINDING_T(sliderH , _myimpl::ColorSliderH );
+    SGRP_DECLARE_BINDING_T(sliderA , _myimpl::ColorSliderA );
+
+    double getColorH() const;
+    double getColorS() const;
+    double getColorV() const;
+
+    void setColorH(double value);
+    void setColorS(double value);
+    void setColorV(double value);
+
+};
+
+
+inline double ColorPicker::getAlpha() const {
+    return sliderA().getValue();
+}
+
+inline void ColorPicker::setAlpha(double value) {
+    sliderA().setValue(value);
+}
+
+
+inline const ColorPicker *_myimpl::ColorSliderSV::getParent() const { return dynamic_cast<const ColorPicker *>(parent); }
+inline       ColorPicker *_myimpl::ColorSliderSV::getParent()       { return dynamic_cast<      ColorPicker *>(parent); }
+inline const ColorPicker *_myimpl::ColorSliderH ::getParent() const { return dynamic_cast<const ColorPicker *>(parent); }
+inline       ColorPicker *_myimpl::ColorSliderH ::getParent()       { return dynamic_cast<      ColorPicker *>(parent); }
+inline const ColorPicker *_myimpl::ColorSliderA ::getParent() const { return dynamic_cast<const ColorPicker *>(parent); }
+inline       ColorPicker *_myimpl::ColorSliderA ::getParent()       { return dynamic_cast<      ColorPicker *>(parent); }
+
+inline _myimpl::ColorSliderSV::ColorSliderSV(ColorPicker *parent_, const Rect<double> &region_,
+                                             const Rect<double> &limits_) :
+    Base(parent_, region_, limits_, {},
+         new ColorSliderThumb(this, region_.getCenter())) {}
+
+inline _myimpl::ColorSliderH::ColorSliderH(ColorPicker *parent_, const Rect<double> &region_,
+                                           double min, double max) :
+    Base(parent_, region_, min, max, 0,
+         new ColorSliderThumb(this, region_.getCenter())) {}
+
+inline _myimpl::ColorSliderA::ColorSliderA(ColorPicker *parent_, const Rect<double> &region_,
+                                           double min, double max) :
+    Base(parent_, region_, min, max, 1,
+         new ColorSliderThumb(this, region_.getCenter())) {}
