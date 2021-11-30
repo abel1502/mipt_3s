665e94889ece282714d9781785d7e4954e3c96c9
abel1502 <terragame.abel@gmail.com>
2021-11-30 12:15:04 +0300

    VS Photoshoop expand tests

--- VS/Photoshoop/app.cpp
+++ VS/Photoshoop/app.cpp
@@ -1,7 +1,8 @@
 #include <AGF/llgui.h>
-#include "app.h"
 #include <AGF/widgets/all.h>
+#include "app.h"
 #include "canvas.h"
+#include "color_picker.h"
 
 
 abel::gui::Application::app_ptr_t abel::gui::Application::create() {
@@ -31,7 +32,8 @@ void MyApp::init(int argc, const char **argv) {
 
     Button &btn1 = grp->createChild<Button>(Rect<double>::wh(20, 20, 100, 60), "Button one");
     btn1.sigClick += [](){
-        DBG("Knopochka"); return false;
+        DBG("Knopochka");
+        return false;
     };
 
     Button &btn2 = grp->createChild<Button>(Rect<double>::wh(20, 90, 100, 60), "Button two");
@@ -45,10 +47,34 @@ void MyApp::init(int argc, const char **argv) {
 
     Canvas *canvas = new Canvas(nullptr, Rect<double>::se(0, 0, 300, 200).pad(10));
 
+    // Slider2D *slider = new Slider2D(nullptr, Rect<double>::wh(20, 20, 100, 100), Rect<double>::se(-25, -25, 25, 25));
+    SliderH *slider = new SliderH(nullptr, Rect<double>::wh(20, 20, 100, 15), -25, 25);
+
+    // slider->sigChanged += [](Vector2d value) {
+    //     DBG("Slider set to %lg", value.x());
+    //
+    //     return false;
+    // };
+
+    ColorPicker *palette = new ColorPicker(nullptr, Rect<double>::wh(0, 0, 120, 175));
+
+    btn2.sigClick += [palette]() {
+        // TODO: Very bad, because palette could have been destroyed
+        Color color = palette->getColor();
+
+        DBG("The color is %f %f %f %f", color.r(), color.g(), color.b(), palette->getAlpha());
+        palette->setColor(color);
+
+        return false;
+    };
+
     WindowManager *mgr = new WindowManager(nullptr, Rect<double>::wh(0, 0, 800, 600));
-    mgr->createWindow(Rect<double>::wh(140, 50, 300, 200), "Sample window", grp);
-    mgr->createWindow(Rect<double>::wh(240, 70, 300, 200), "Sample window 2", canvas);
-    mgr->createWindow(Rect<double>::wh(80, 90, 300, 200),  "Sample window 3", new Window::EmptyBody());
+    mgr->createWindow(Rect<double>::wh(140,  50, 300, 200), "Some buttons", grp);
+    mgr->createWindow(Rect<double>::wh(240,  70, 300, 200), "A drawing canvas", canvas);
+    mgr->createWindow(Rect<double>::wh( 80,  90, 300, 200), "A slider", slider /*new Window::EmptyBody()*/);
+    mgr->createWindow(Rect<double>::wh(290, 150, 300, 200), "A color picker, woah!", palette);
+
+    palette->setColor(Color::WHITE);
 
 
     mainWidget = mgr;
