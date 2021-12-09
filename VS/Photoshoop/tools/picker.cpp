b8541d4115fd62e72bda80ad78ffba1467020a28
abel1502 <terragame.abel@gmail.com>
2021-12-09 23:18:06 +0300

    VS Photoshoop fix picker tool

--- VS/Photoshoop/tools/picker.cpp
+++ VS/Photoshoop/tools/picker.cpp
@@ -9,5 +9,9 @@ void PickerTool::applyLine(Layer &layer, const Vector2d &pos0, const Vector2d &p
 void PickerTool::applyPoint(Layer &layer, const Vector2d &pos) {
     abel::gui::Texture &target = layer.getTexture();
 
+    if (!target.getRect().padded(1).contains(pos)) {
+        return;
+    }
+
     getManager().setColor(Color{target.getPixelColor(pos)});
 }
